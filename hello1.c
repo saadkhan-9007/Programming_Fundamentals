6e9e3966f503497cb5401c45190430e58bb82351
saad9007 <p229007@pwr.nu.edu.pk>
2022-09-14 18:00:24 +0500

    Hello World1

---
+++ hello1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int x, y, result;
+  printf("Enter a number : ");
+  scanf("%d", &x);
+  printf("Enter another number : ")'
+  scanf("%d", &y);
+  result = x + y;
+  printf("sum of 2 numbers =  %d", x, y);
+  return 0;
+}
